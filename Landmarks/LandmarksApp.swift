4ed2453d08692da0de6bba2ba3c2a620bbdb0d3a
suzuki.masayoshi <suzuki.masayoshi@moneyforward.co.jp>
2022-08-01 18:41:02 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by suzuki.masayoshi on 2022/08/01.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
