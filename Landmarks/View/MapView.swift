667bb5bf34ed91b15e910e7c4acb70d24ff38f50
suzuki.masayoshi <suzuki.masayoshi@moneyforward.co.jp>
2022-08-17 19:56:49 +0900

    Create Landmarks file.

---
+++ Landmarks/View/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by suzuki.masayoshi on 2022/08/01.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868),
+        span: MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
